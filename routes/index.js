6028dad3a1498d3bf25ecd9fd35c4b855cd1e76f
admirty <admirty1031@gmail.com>
2017-03-30 01:42:00 +0800

    ytr

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,6 @@
 var express = require('express');
 var router = express.Router();
 
-var linebot = require('linebot');
-var express = require('express');
-
-var bot = linebot({
-    channelId: 1505555353,
-    channelSecret: d77d29c78fd97d14a10a483ec924d433,
-    channelAccessToken: V2TrUMDraUjqc8d / xnT0Zc41vhSRYmd + 8njajWaekZ2B3jqttnyRZ5XrKRt37tUX0TmziPPYDJlrFk8/WhxLA4vL650Bv7c2gWoeHy/kdAZ7XIk3MNdm9M0nqhD6LALazU2PE9PC2vXKrRw/ ZRpF7QdB04t89 / 1O/w1cDnyilFU=
-});
-
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Expres5555s' });
